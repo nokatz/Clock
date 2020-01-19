942ed8a71d4dac637e7faaf52bb3d03021b03dac
nokatz <nokacpetrusicki@yahoo.com>
2020-01-19 19:49:33 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+﻿
+let UI = {};
+
+getDomElements();
+
+displayTime();
+
+function displayTime(){
+	
+	let d = new Date();
+	let s = d.getSeconds(), 
+		m = d.getMinutes(), 
+		h = d.getHours();
+
+	if(s < 10)
+		s = "0" + s;
+	if(m < 10)
+		m = "0" + m;
+		
+	UI.clock.textContent = h + ':' + m + ':' + s;
+	
+	setInterval(displayTime, 1000);
+}
+
+function getDomElements(){
+	UI.clock = document.getElementById('clock');
+}
