f91ff5bb94a1509e39212e00f419cb2ab12de82c
nokatz <nokacpetrusicki@yahoo.com>
2020-01-30 18:22:07 +0100

    clock or timer option

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
 ﻿
 let UI = {};
+let s, m, h, d, t, t0;
+let clock = true;
 
 getDomElements();
+
+t0 = new Date();
 
 displayTime();
 
 function displayTime(){
 	
-	let d = new Date();
-	let s = d.getSeconds(), 
-		m = d.getMinutes(), 
-		h = d.getHours();
-
-	if(s < 10)
-		s = "0" + s;
-	if(m < 10)
-		m = "0" + m;
+	if(clock){		
+	
+		d = new Date();			
+		h = ("0" + d.getHours()).slice(-2);
+	
+	} else {	// timer, start from zero
+	
+		t = new Date() - t0;
+		d = new Date(t);	
+		
+		h = ("0" + d.getUTCHours()).slice(-2);		
+	}
+				
+	s = ("0" + d.getSeconds()).slice(-2);
+	m = ("0" + d.getMinutes()).slice(-2);
 		
 	UI.clock.textContent = h + ':' + m + ':' + s;
 	
 	setInterval(displayTime, 1000);
+	
 }
 
 function getDomElements(){
